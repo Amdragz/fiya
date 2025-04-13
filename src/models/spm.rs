e3855fc2459f856a7e225685b14411eac0f7df80
Amdragz <ogaji2006@gmail.com>
2025-04-13 17:06:46 +0100

    feat(spm): added middleware check for spm device post to update cage info

--- src/models/spm.rs
+++ src/models/spm.rs
@@ -20,6 +20,25 @@ pub struct Cage {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CageWithDeviceToken {
+    #[serde(rename = "_id")]
+    pub id: String,
+    pub device_token: String,
+    pub assigned_monitor: String,
+    pub livestock_no: u32,
+    pub temperature: f32,
+    pub humidity: f32,
+    pub pressure: f32,
+    pub ammonia: f32,
+    pub co2: u32,
+    pub object_recognition: ObjectRecognition,
+    #[serde(with = "bson::serde_helpers::chrono_datetime_as_bson_datetime")]
+    pub created_at: DateTime<Utc>,
+    #[serde(with = "bson::serde_helpers::chrono_datetime_as_bson_datetime")]
+    pub updated_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ObjectRecognition {
     pub coccidiosis: f32,
     pub newcastle: f32,
@@ -35,6 +54,17 @@ pub struct UpdateCage {
     pub ammonia: f32,
     pub co2: u32,
     pub object_recognition: ObjectRecognition,
+    #[serde(with = "bson::serde_helpers::chrono_datetime_as_bson_datetime")]
+    pub updated_at: DateTime<Utc>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct SpmDeviceToken {
+    #[serde(rename = "_id")]
+    pub id: String,
+    pub token: String,
+    #[serde(with = "bson::serde_helpers::chrono_datetime_as_bson_datetime")]
+    pub created_at: DateTime<Utc>,
     #[serde(with = "bson::serde_helpers::chrono_datetime_as_bson_datetime")]
     pub updated_at: DateTime<Utc>,
 }
