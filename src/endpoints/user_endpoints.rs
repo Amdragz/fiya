67b2ca184023ae187e3e9dac483be073a17fd240
Amdragz <ogaji2006@gmail.com>
2025-04-15 21:12:33 +0100

    fix(auth): moved the get authenticated user endpoint to the auth endpoints

--- src/endpoints/user_endpoints.rs
+++ src/endpoints/user_endpoints.rs
@@ -1,15 +1,13 @@
 use axum::{
     extract::{Path, State},
-    middleware,
-    routing::{get, post},
-    Extension, Router,
+    routing::post,
+    Router,
 };
 use std::sync::Arc;
 
 use crate::{
     dtos::user::{CreateAdminUserDto, CreateCustomerDto},
-    middleware::auth_middleware,
-    models::user::{AuthUserDto, NewUser},
+    models::user::NewUser,
     services::user_service::UserService,
     utils::{
         response::{ApiErrorResponse, ApiSuccessResponse},
@@ -22,10 +20,6 @@ pub fn user_endpoints() -> Router<Arc<AppState>> {
     Router::new()
         .route("/", post(create_admin_user))
         .route("/:id/customer", post(create_customer_user))
-        .route(
-            "/:id",
-            get(get_authenticated_user).layer(middleware::from_fn(auth_middleware::requires_auth)),
-        )
 }
 
 async fn create_admin_user(
@@ -43,12 +37,4 @@ async fn create_customer_user(
 ) -> Result<ApiSuccessResponse<NewUser>, ApiErrorResponse> {
     let user_service = UserService::new(app_state.mongo_client.clone());
     user_service.create_customer_user(admin_id, payload).await
-}
-
-async fn get_authenticated_user(
-    State(app_state): State<Arc<AppState>>,
-    Extension(auth_user): Extension<AuthUserDto>,
-) -> Result<ApiSuccessResponse<NewUser>, ApiErrorResponse> {
-    let user_service = UserService::new(app_state.mongo_client.clone());
-    user_service.get_authenticated_user(auth_user.id).await
 }
